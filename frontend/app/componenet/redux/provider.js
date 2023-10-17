4245921a7895f9830d8bf8dc8dfc8f4da445e8ed
kiyotone <kunwarkirtan36@gmail.com>
2023-10-17 13:47:10 +0545

    redux persist added

--- frontend/app/componenet/redux/provider.js
+++ frontend/app/componenet/redux/provider.js
@@ -1,12 +1,16 @@
 "use client";
+import { PersistGate } from 'redux-persist/integration/react'
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { persistStore } from 'redux-persist';
 
 export function ReduxProvider({ children }) {
   return (
     <Provider store={store}>
-      
+      <PersistGate persistor={persistStore(store)}>
+
         {children}
+      </PersistGate>
     
     </Provider>
   
--- frontend/app/componenet/redux/store.js
+++ frontend/app/componenet/redux/store.js
@@ -1,9 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import mainReducer from "./mainSlice";
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistReducer } from 'redux-persist';
+
+const reducers = combineReducers({
+  main : mainReducer,
+
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const persistedReducer = persistReducer(persistConfig, reducers)
 
 export const store = configureStore({
-  reducer: {
-    main: mainReducer,
-  },
+reducer :  persistedReducer,
+middleware: (getDefaultMiddleware) => {
+  return getDefaultMiddleware({
+    serializableCheck: false,
+  });
+},
 });
